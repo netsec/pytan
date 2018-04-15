f639d68eed8f4d21b33d3a453632133d1f49e92f
Jim Olsen <jim.olsen@tanium.com>
2018-04-14 21:17:01 -0400

    clean up test config file for #43

--- test/API_INFO.py
+++ test/API_INFO.py
@@ -1,18 +1,16 @@
+"""Handler and testing configuration variables."""
+
 SERVER_INFO = {
+    # pytan handler arguments
     "username": "Administrator",
     "password": "Tanium2015!",
-    "host": "doomsday",  # 6.5
-    # "host": "172.16.31.128",  # 6.2
+    "host": "172.16.30.130",
     "port": "443",
-    # control level of logging for pytan
-    "loglevel": 1,
-    # control level of logging / verbosity for unittest
-    "testlevel": 3,
     "debugformat": False,
-    # have unittest exit immediately on unexpected error
-    'FAILFAST': True,
-    # catch control-C to allow current test suite to finish (press 2x to force)
-    'CATCHBREAK': True,
-    # only show output from failed tests
-    'BUFFER': False,
+    "loglevel": 1,  # control level of logging for pytan
+    # testing arguments
+    "testlevel": 3,  # control level of logging / verbosity for unittest
+    'FAILFAST': False,  # have unittest exit immediately on unexpected error
+    'CATCHBREAK': True,  # catch control-C to allow current test suite to finish (press 2x to force)
+    'BUFFER': False,  # only show output from failed tests
 }
